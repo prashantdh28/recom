66634c9d59d4419b457d8b8f5fceb4dfb28d8403
Parth Hala <parthhala@topsinfosolution.com>
2025-05-08 12:29:26 +0530

    add scheduler

--- resources/views/transparency/account-config/index.blade.php
+++ resources/views/transparency/account-config/index.blade.php
@@ -38,52 +38,6 @@
         </div>
 
         <!-- DRAWER COMPONENT -->
-        {{-- <div id="drawer-contact" @click.outside="toggleDrawer = false"
-            class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-white w-80 dark:bg-gray-800"
-            :class="toggleDrawer ? 'drawer -translate-x-full': 'translate-x-full'" tabindex="-1" aria-labelledby="drawer-contact-label">
-            <h5 id="drawer-label"
-                class="inline-flex items-center mb-6 text-base font-semibold text-gray-500 uppercase dark:text-gray-400">
-                Filter
-            </h5>
-            <button type="button" @click="toggleDrawer = !toggleDrawer" data-drawer-hide="drawer-contact" aria-controls="drawer-contact"
-                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-                <span class="sr-only">Close menu</span>
-            </button>
-            <form name='advance_filter_form' id='advance-filter' onsubmit='return false' method="POST">
-                @csrf
-                
-                <div class="card-body">
-                    <div class="card-body p-0">
-                        <div class="accordion" id="accordionExample">
-                            <div class="accordion-item filterform border-0">
-
-                                <div class="mb-5">
-                                    <label class="form-label">Status</label>
-                                    <select class="select" name="status">
-                                        <option value="">--Select--</option>
-                                        @forelse ($status as $key => $value)
-                                            <option value="{{ $value }}">{{ $key }}</option>
-                                        @empty
-                                        @endforelse
-                                    </select>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer text-end py-3 fixed right-0 bottom-0 space-x-2">
-                    <button class="btn btn-light" type="button" id="advance-filter-reset">Reset</button>
-                    <button class="btn btn-primary" type="submit" id="advance-submit" name="submit">Apply</button>
-                </div>
-            </form>
-        </div> --}}
-
         <div class="drawer drawer-end flex flex-col max-w-[90%] w-[400px]" data-drawer="true" id="filter-sidebar">
             <div class="flex items-center justify-between p-5 border-b">
                 <h3 class="text-base font-semibold text-gray-900">
